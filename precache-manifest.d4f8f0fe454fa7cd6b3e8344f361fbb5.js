200b6fc680b65813ef013c209ce6829e6aeddd23
Daniel Callander <knifacat@gmail.com>
2020-08-17 02:16:52 +0100

    Updates

---
+++ precache-manifest.d4f8f0fe454fa7cd6b3e8344f361fbb5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a9b7a834d346c44b8e99d9b4d9f99fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d0a0234343b42e80747",
+    "url": "/static/css/main.e99877f2.chunk.css"
+  },
+  {
+    "revision": "c04c68f379dcf7b161bb",
+    "url": "/static/js/2.cfb49ab5.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.cfb49ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0a0234343b42e80747",
+    "url": "/static/js/main.77348da0.chunk.js"
+  },
+  {
+    "revision": "e9dadb46b703ffded895",
+    "url": "/static/js/runtime-main.35488dec.js"
+  },
+  {
+    "revision": "878cd0955ca1c0a95ad49d79c193022d",
+    "url": "/static/media/cloud.878cd095.svg"
+  },
+  {
+    "revision": "9f6f7528fb5e13db85c0cdb7fc98e37b",
+    "url": "/static/media/sky_back.9f6f7528.svg"
+  },
+  {
+    "revision": "d4980b59aad360178f1e7d55a329acbf",
+    "url": "/static/media/sky_front.d4980b59.svg"
+  }
+]);
