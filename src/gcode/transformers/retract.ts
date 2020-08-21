a810638eeedab658f856ccbc59c857c1c4506de9
Daniel Callander <knifacat@gmail.com>
2020-08-21 02:29:48 +0100

    Fix retractions for Cura

--- src/gcode/transformers/retract.ts
+++ src/gcode/transformers/retract.ts
@@ -34,7 +34,7 @@ export class RetractTransformer extends GCodeTransformer {
     this.speedRange = opts.speedRange;
     this.retractRegex = opts.retractRegex
       ? opts.retractRegex
-      : /G(0|1) E(?<distance>(-|\d|\.)+) F(?<speed>(\d|.)+)/;
+      : /G(0|1)((?=.*\bE(?<distance>(-|\d|\.)+)\b)(?=.*\bF(?<speed>(\d|\.)+)\b))(?!.*(X|Y|Z))/;
 
     this.retractMode = RetractMode.Absolute;
 
